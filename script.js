e1c3e1408227d98dfaa2cd2012894d8a7289a827
tobiasb4c <tobias.nahler@b4c-consulting.com>
2023-01-09 11:51:47 +0100

    v1.0.0

--- script.js
+++ script.js
@@ -7,32 +7,10 @@ fetch('/OpenJsonData.json')
 
     var random = (Math.floor(Math.random() * json.length))
     console.log(random);
-    document.getElementById('fetchedData').innerText = 'Titel: ' + json[random].Titel ;
-    document.getElementById('fetchedData2').innerText = 'Place: ' + json[random].Place ;
-    document.getElementById('fetchedData3').innerText = 'Decription: ' + json[random].Decription ;
+    document.getElementById('fetchedData').innerHTML = 'Titel: ' + json[random].Titel ;
+    document.getElementById('fetchedData2').innerHTML = 'Place: ' + json[random].Place ;
+    document.getElementById('fetchedData3').innerHTML = 'Description: ' + json[random].Description ;
     });
 
     console.log(jsonA);
 }
-
-
-
-//console.log(jsonA.toString());
-
-
-
-//document.getElementById('fetchedData').innerText = jsonA[random].toString() ;
-
-    /*
-fetch('http://opendata.wifi.at/OpenJsonData.json', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-    },
-})
-.then(response => response.json())
-.then(response => document.getElementById('fetchedData').innerText = (JSON.stringify(response)));
-  
-/*function fetch(){
-    
-}*/
